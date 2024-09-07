a296794aab4d204c4710b91c33e82d70e6df5a08
njwmerv <montoya.nicanor04@gmail.com>
2024-09-07 17:20:37 -0400

    First commit.

---
+++ components/PillButton.tsx
@@ -0,0 +1,30 @@
+import {Pressable, StyleSheet, Text} from 'react-native';
+
+export default function PillButton({label,
+                                    onPress,
+                                    textStyle,
+                                    buttonStyle}){
+
+    // Render
+
+    return (
+      <Pressable style={[styles.pressable, buttonStyle]}
+                 onPress={onPress}>
+          <Text style={[styles.text, textStyle]}>{label}</Text>
+      </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    pressable:{
+        width:'fit-content',
+        margin:'auto',
+        padding:10,
+        borderRadius:18,
+        backgroundColor:'orange'
+    },
+    text:{
+        color:'#FFFFFF',
+        fontSize:24
+    }
+});
