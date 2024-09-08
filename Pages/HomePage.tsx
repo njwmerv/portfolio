1ab16af2882f8aa8ec2a4f970594e9ada837081d
njwmerv <montoya.nicanor04@gmail.com>
2024-09-07 23:51:25 -0400

    Switched sidebar to be on top of screen and added image to JumanPing cell.

--- Pages/HomePage.tsx
+++ Pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import {Image, Text, StyleSheet, View, Pressable} from 'react-native';
+import {Image, Text, StyleSheet, View, Pressable, useWindowDimensions} from 'react-native';
 import PillButton from "@/components/PillButton";
 import PreviewCell from "@/components/PreviewCell";
 
@@ -6,11 +6,15 @@ export default function HomePage({}){
 
     // Instance Variables
 
+    const {height} = useWindowDimensions();
+
     const projectsList = [
         {title:'Tic-Tac-Toe', imageUri:'https://i.imgur.com/2LWzlBx.png', description:'Just simple tic-tac-toe implemented in Python, playable in the command line.'},
         {title:'Pong', imageUri:'https://i.imgur.com/hC5SI1R.png', description:'Pong recreated in Pygame, where you can 1v1 your friend.'},
-        {title:'Juman Ping', imageUri:'', description:'2D platformer game for the PC, where players create and break their own platforms.'}
+        {title:'Juman Ping', imageUri:'https://i.imgur.com/8EQd0dK.png', description:'2D platformer game for the PC, where players create and break their own platforms.'}
     ];
+
+    const navBarHeight = 75;
 
     // Methods
 
@@ -22,30 +26,24 @@ export default function HomePage({}){
 
     return (
         <View style={styles.mainContainer}>
-            <View style={styles.navBar}>
-                <Image style={styles.profilePicture}
-                       source={{uri:'https://i.imgur.com/41v9YF4.jpeg'}}
-                />
-
-                <View style={styles.profileTextContainer}>
-                    <Text style={[styles.text, styles.headerText, styles.profileText]}>Nicanor Montoya</Text>
-                    <Text style={[styles.text, styles.profileText]}>Developer</Text>
-                </View>
-
+            <View style={[styles.navBar, {height:navBarHeight}]}>
                 <View style={styles.linksContainer}>
-                    <Pressable onPress={() => openInNewTab('https://github.com/njwmerv')}>
+                    <Pressable style={styles.link}
+                               onPress={() => openInNewTab('https://github.com/njwmerv')}>
                         <Image style={styles.link}
                                source={{uri: 'https://i.imgur.com/6VekIvR.png'}}
                         />
                     </Pressable>
 
-                    <Pressable onPress={() => openInNewTab('https://www.linkedin.com/in/nicanor-montoya-63029a255/')}>
+                    <Pressable style={styles.link}
+                               onPress={() => openInNewTab('https://www.linkedin.com/in/nicanor-montoya-63029a255/')}>
                         <Image style={styles.link}
                                source={{uri: 'https://i.imgur.com/CdaktLT.png'}}
                         />
                     </Pressable>
 
-                    <Pressable onPress={() => openInNewTab('mailto:montoya.nicanor04@gmail.com')}>
+                    <Pressable style={styles.link}
+                               onPress={() => openInNewTab('mailto:montoya.nicanor04@gmail.com')}>
                         <Image style={styles.link}
                                source={{uri: 'https://i.imgur.com/Kjti7Je.png'}}
                         />
@@ -53,18 +51,27 @@ export default function HomePage({}){
                 </View>
 
                 <View style={styles.sectionLinksContainer}>
-
+                    <Text>hi</Text>
                 </View>
             </View>
 
-            <View style={styles.contentContainer}>
-                <View style={styles.contentSection}>
+            <View style={[styles.contentContainer, {height:height - navBarHeight}]}>
+                <View style={[styles.contentSection, {height:height - navBarHeight - 20}]}>
                     <Text style={[styles.text, styles.headerText]}>About Me</Text>
 
                     <Text style={[styles.text]}>Lorem Ipsum dolor sit amet consectetur.</Text>
+
+                    <Image style={styles.profilePicture}
+                           source={{uri:'https://i.imgur.com/41v9YF4.jpeg'}}
+                    />
+
+                    <View style={styles.profileTextContainer}>
+                        <Text style={[styles.text, styles.headerText, styles.profileText]}>Nicanor Montoya</Text>
+                        <Text style={[styles.text, styles.profileText]}>Developer</Text>
+                    </View>
                 </View>
 
-                <View style={styles.contentSection}>
+                <View style={[styles.contentSection, {height:height - navBarHeight}]}>
                     <Text style={[styles.text, styles.headerText]}>Projects</Text>
 
                     <Text style={[styles.text]}>Here's some of the stuff that I worked on.</Text>
@@ -85,7 +92,7 @@ export default function HomePage({}){
                     />
                 </View>
 
-                <View style={[styles.contentSection, {marginBottom:0}]}>
+                <View style={[styles.contentSection, {height:height - navBarHeight - 20}]}>
                     <Text style={[styles.text, styles.headerText]}>Experience</Text>
 
                     <Text style={[styles.text]}>Lorem Ipsum dolor sit amet consectetur.</Text>
@@ -106,15 +113,16 @@ const styles = StyleSheet.create({
     mainContainer:{
         width:'100%',
         height:'100%',
-        flexDirection:'row',
+        flexDirection:'column',
         backgroundColor:'#112D4E'
     },
     navBar:{
         gap:15,
-        flex:1,
-        height:'100%',
+        width:'100%',
         padding:10,
-        flexDirection:'column',
+        alignItems:'center',
+        flexDirection:'row',
+        justifyContent:'space-between',
         backgroundColor:'#3F72AF'
     },
     profilePicture:{
@@ -133,7 +141,9 @@ const styles = StyleSheet.create({
         lineHeight:28
     },
     linksContainer:{
-        width:'100%',
+        gap:15,
+        height:40,
+        marginLeft:15,
         flexDirection:'row',
         justifyContent:'space-evenly'
     },
@@ -145,14 +155,12 @@ const styles = StyleSheet.create({
 
     },
     contentContainer:{
-        flex:7,
         padding:20,
         overflowY:'scroll'
     },
     contentSection:{
         width:'100%',
-        height:'100%',
-        marginBottom:20
+        height:'100%'
     },
     projectsList:{
         margin:'auto',
