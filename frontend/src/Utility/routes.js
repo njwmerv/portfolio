c411d3e87c69f73eb3d9c4d3dc124b8c2c94df04
Mari Montoya <kulafumon@gmail.com>
2025-03-01 17:12:27 -0500

    Setup dotenv for frontend.

--- frontend/src/Utility/routes.js
+++ frontend/src/Utility/routes.js
@@ -3,7 +3,7 @@ export const HOME_PAGE_ROUTE = '/';
 export const PROJECT_PAGE_ROUTE = '/projects';
 
 // API
-export const API_URL = 'http://localhost:3000';
+export const API_URL = import.meta.env.VITE_BACKEND_API_URL;
 export const GET_PROJECTS_ROUTE = '/projects';
 export const GET_TOP_THREE_PROJECTS = '/top';
 export const GET_TAGS_ROUTE = '/tags';
