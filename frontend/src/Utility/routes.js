b699c2a3d13be1ee4d2dd3d76aa2f9455f3a5081
Mari Montoya <kulafumon@gmail.com>
2025-03-01 16:20:12 -0500

    Added new routes for endpoints.

--- frontend/src/Utility/routes.js
+++ frontend/src/Utility/routes.js
@@ -1,2 +1,9 @@
+// Router
 export const HOME_PAGE_ROUTE = '/';
 export const PROJECT_PAGE_ROUTE = '/projects';
+
+// API
+export const API_URL = 'http://localhost:3000';
+export const GET_PROJECTS_ROUTE = '/projects';
+export const GET_TOP_THREE_PROJECTS = '/top';
+export const GET_TAGS_ROUTE = '/tags';
