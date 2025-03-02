a3e219d0e3b8af3f9082ad4d442199759288d45e
Mari Montoya <kulafumon@gmail.com>
2025-03-01 21:11:26 -0500

    Again...

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -20,7 +20,6 @@ export default function ProjectPage(){
 
 	async function fetchProjects(){
 		const URL = `${API_URL}${GET_PROJECTS_ROUTE}`;
-		console.log('MARI APIURL', URL);
 		const response = await fetch(URL);
 		if(!response.ok){
 			console.log('Failed to fetch projects:', response.statusText);
